454836df3a990fa56a408fe3872854f3bef41f54
Adrian Ronquillo <ronquiaj@plu.edu>
2021-02-12 13:50:57 -0900

    --

--- src/styles/archivedStoriesStyles.js
+++ src/styles/archivedStoriesStyles.js
@@ -7,7 +7,18 @@ const archiveStyles = {
     width: "30vw",
     webkitBoxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
     mozBoxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
-    boxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset"
+    boxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
+    animation: "fade-in .7s"
+  },
+  "@global": {
+    "@keyframes fade-in": {
+      "0%": {
+        opacity: "0"
+      },
+      "100%": {
+        opacity: "1"
+      }
+    }
   },
   titleLink: {
     color: "white"
