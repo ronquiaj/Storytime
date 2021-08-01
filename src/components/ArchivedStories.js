5b23bfeeb6a921ca1892a535bac6fd44c31e2b4e
Adrian Ronquillo <ronquiaj@plu.edu>
2021-08-01 10:53:02 -0500

    Finally updated this website lol. The archive story page should now properly display the cards instead of having them look messy on big screens

--- src/components/ArchivedStories.js
+++ src/components/ArchivedStories.js
@@ -4,33 +4,55 @@ import { db } from "../firebase/Firebase";
 import MiniStory from "./MiniStory";
 import styles from "../styles/archivedStoriesStyles";
 import Spinner from "./Spinner";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function ArchivedStories(props) {
   const [archivedPosts, setArchivedPosts] = useState([]);
   const [loading, changeLoading] = useState(true);
   const { classes } = props;
-
+  const bigScreen = useMediaQuery("(min-width:1025px)");
   // method to fetch data from the database and be used in the use effect to display archived posts
   const fetchData = useCallback(async () => {
     const archiveRef = db.collection("archive");
     const archiveData = await archiveRef.get();
     const archive = [];
-    archiveData.forEach((story) => {
-      const { title, text, emoji, dateCreated } = story.data();
-      archive.push(
-        <MiniStory
-          title={title}
-          text={text}
-          emoji={emoji}
-          classes={classes}
-          dateCreated={dateCreated}
-          key={title}
-        />
-      );
-    });
+    let row = [];
+    if (bigScreen)
+      archiveData.forEach((story) => {
+        const { title, text, emoji, dateCreated } = story.data();
+        if (row.length === 3) {
+          archive.push(<div className={classes.archiveRow}>{row}</div>);
+          row = [];
+        } else
+          row.push(
+            <MiniStory
+              title={title}
+              text={text}
+              emoji={emoji}
+              classes={classes}
+              dateCreated={dateCreated}
+              key={title}
+            />
+          );
+      });
+    else
+      archiveData.forEach((story) => {
+        const { title, text, emoji, dateCreated } = story.data();
+        archive.push(
+          <MiniStory
+            title={title}
+            text={text}
+            emoji={emoji}
+            classes={classes}
+            dateCreated={dateCreated}
+            key={title}
+          />
+        );
+      });
+    if (row.length > 0) archive.push(row);
     setArchivedPosts(archive);
     changeLoading(false);
-  }, [classes]);
+  }, [classes, bigScreen]);
 
   //useeffect to fetch arhive data from the database
   useEffect(() => {
@@ -42,7 +64,9 @@ function ArchivedStories(props) {
       {loading ? (
         <Spinner />
       ) : archivedPosts.length === 0 ? (
-        <h1 className={classes.noArchivedPosts}>There are no archived posts yet!</h1>
+        <h1 className={classes.noArchivedPosts}>
+          There are no archived posts yet!
+        </h1>
       ) : (
         <div className={classes.archiveContainer}>{archivedPosts}</div>
       )}
--- src/styles/archivedStoriesStyles.js
+++ src/styles/archivedStoriesStyles.js
@@ -5,89 +5,103 @@ const archiveStyles = {
     backgroundColor: "white",
     padding: 0,
     width: "30vw",
-    webkitBoxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
-    mozBoxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
-    boxShadow: "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
-    animation: "fade-in .7s"
+    webkitBoxShadow:
+      "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
+    mozBoxShadow:
+      "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
+    boxShadow:
+      "0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset",
+    animation: "fade-in .7s",
   },
   "@global": {
     "@keyframes fade-in": {
       "0%": {
-        opacity: "0"
+        opacity: "0",
       },
       "100%": {
-        opacity: "1"
-      }
-    }
+        opacity: "1",
+      },
+    },
   },
   titleLink: {
-    color: "white"
+    color: "white",
   },
   button: {
     transition: "0.6s all",
     "&:hover a": {
       textDecoration: "none",
       color: "white",
-      margin: 0
+      margin: 0,
     },
     "&:hover": {
       width: "11rem",
       height: "3.3rem",
-      margin: 0
-    }
+      margin: 0,
+    },
   },
   submit: {
     width: "100%",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   container: {
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   noArchivedPosts: {
-    marginTop: "3rem"
+    marginTop: "3rem",
   },
   emoji: {
     display: "flex",
     fontSize: "1.80rem",
     justifyContent: "flex-end",
     alignSelf: "flex-end",
-    cursor: "pointer"
+    cursor: "pointer",
   },
   archiveContainer: {
+    width: "100%",
     display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
-    justifyContent: "space-around",
-    margin: "2rem",
-    flexFlow: "row wrap"
+  },
+  archiveRow: {
+    width: "90%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   "@media (min-width: 768px)": {
+    archiveContainer: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-around",
+      margin: "2rem",
+      flexFlow: "row wrap",
+    },
     storyCard: {
       width: "22rem",
-      margin: "3rem 0"
+      margin: "3rem 0",
     },
     button: {
       height: "3rem",
       padding: "0.2rem",
-      width: "10rem"
-    }
+      width: "10rem",
+    },
   },
   "@media (max-width: 820px)": {
-    archiveContainer: {
-      marginLeft: "1.6rem"
-    },
     storyCard: {
       width: "80vw",
       margin: "3rem 0",
-      padding: "0.7rem"
+      padding: "0.7rem",
     },
     button: {
       height: "3rem",
       padding: "0.2rem",
-      width: "10rem"
-    }
-  }
+      width: "10rem",
+    },
+  },
 };
 
 export default archiveStyles;
